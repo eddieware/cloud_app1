821b6ebf8a4075dde8153812b59e01b8aabc9ec4
eddieware <47250727+eddieware@users.noreply.github.com>
2020-07-19 22:04:38 -0500

    update_final_part

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -15,13 +16,25 @@ class DemoInfo {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<DemoInfo> _bandList = [
-    DemoInfo(name: 'Nombre demo 1', votes: 1),
-    DemoInfo(name: 'Nombre demo 2', votes: 2),
-    DemoInfo(name: 'Nombre demo 3', votes: 3),
-    DemoInfo(name: 'Nombre demo 4', votes: 4),
-    DemoInfo(name: 'Nombre demo 4', votes: 5),
-  ];
+  // final List<DemoInfo> _bandList = [
+  //   DemoInfo(name: 'Nombre demo 1', votes: 1),
+  //   DemoInfo(name: 'Nombre demo 2', votes: 2),
+  //   DemoInfo(name: 'Nombre demo 3', votes: 3),
+  //   DemoInfo(name: 'Nombre demo 4', votes: 4),
+  //   DemoInfo(name: 'Nombre demo 4', votes: 5),
+  // ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    Firestore.instance
+        .collection('bandnames')
+        //.where("topic", isEqualTo: "flutter")
+        .snapshots()
+        .listen((data) => data.documents.forEach((doc) => print(doc['name'])));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -32,26 +45,36 @@ class _HomeScreenState extends State<HomeScreen> {
               largeTitle: Text('Band Names Survey'),
             ),
             SliverSafeArea(
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  return _buildListItem(context, _bandList[index]);
-                }, childCount: _bandList.length),
-              ),
-            )
+                sliver: StreamBuilder<QuerySnapshot>(
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: CupertinoActivityIndicator(),
+                  );
+                return SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    return _buildListItem(
+                        context, snapshot.data.documents[index]);
+                  }, childCount: snapshot.data.documents.length),
+                );
+              },
+              stream: Firestore.instance.collection('bandnames').snapshots(),
+            ))
           ],
         ),
       ),
     );
   }
 
-  Widget _buildListItem(BuildContext context, DemoInfo bandInfo) {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return ListTile(
-      title: Text(bandInfo.name),
+      title: Text(document['name']),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text(bandInfo.votes.toString()),
+          Text(document['votes']),
           Icon(CupertinoIcons.right_chevron),
         ],
       ),
